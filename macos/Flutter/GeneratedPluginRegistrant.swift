bb4a6ab73283eb44c71b5a7ca9d4af09421facec
Victor Carreras <34163765+vicajilau@users.noreply.github.com>
2026-06-22 11:34:43 +0200

    chore: update iOS deployment target, configure scene manifest, and refresh project build settings

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,6 @@ import file_picker
 import file_selector_macos
 import open_file_mac
 import package_info_plus
-import path_provider_foundation
 import pdf_combiner
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -21,6 +20,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PdfCombinerPlugin.register(with: registry.registrar(forPlugin: "PdfCombinerPlugin"))
 }
